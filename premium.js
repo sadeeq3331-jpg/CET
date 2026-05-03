a5d561334c52ea6209d1707ca489a14a1f0cdfc1
Abubakar Sadeq <sadeeq3331@gmail.com>
2026-05-03 15:27:35 +0800

    Update premium.js

--- premium.js
+++ premium.js
@@ -1,6 +1,6 @@
 // ==================== PREMIUM SYSTEM (Google Sheets backend) ====================
 (function() {
-  const API_ENDPOINT = "AKfycbyj2jtdkhCZhgUvl2-F19OC9nB7Br6GS9uRp8NvoHLz";  // ← CHANGE THIS
+  const API_ENDPOINT = "https://docs.google.com/spreadsheets/d/1rqgFtq_ABgEFwb8wVUBBWgP59GtILCvmR2kkfTDohiQ/edit?usp=sharing";  // ← CHANGE THIS
 
   const CONTACT = {
     email: "sadeeq3331@gmail.com",
